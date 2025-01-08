64695df7b2a96ecfa1c1c3e00ed4a76adcc5c921
gdfsjoikgndf <frontend-computer@proton.me>
2025-01-08 23:08:14 +0700

    Add files via upload

---
+++ lib.bat
@@ -0,0 +1,3 @@
+@echo off
+
+pip install pystyle requests faker beautifulsoup4 colorama termcolor aiohttp telebot psutil deep-translator pycryptodome qrcode pytube py7zr torrentool wikipedia fake_useragent
